a7a9dbf1fe21d5b3bc8d63646102d032765c7df5
Jose <jose.for.dev@gmail.com>
2022-03-10 18:06:05 +0000

    Feature - Add load testing using artillery and forever

--- tests/load.test.processor.js
+++ tests/load.test.processor.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const { faker } = require("@faker-js/faker");
+const { faker } = require('@faker-js/faker');
 
-function generateRide(requestParams, ctx, ee, next) {
-  ctx.vars["start_lat"] = faker.datatype.number({ min: -90, max: 90 }).toString();
-  ctx.vars["start_long"] = faker.datatype.number({min: -180,max: 180}).toString();
-  ctx.vars["start_long"] = faker.datatype.number({min: -90,max: 90}).toString();
-  ctx.vars["end_long"] = faker.datatype.number({min: -180,max: 180}).toString();
-  ctx.vars["rider_name"] = faker.name.findName();
-  ctx.vars["driver_name"] = faker.name.findName();
-  ctx.vars["driver_vehicle"] = faker.vehicle.vehicle();
-  return next();
+function generateRide(userContext, events, next) {
+    userContext.vars.start_lat = faker.datatype.number({ min: -90, max: 90 });
+    userContext.vars.start_long = faker.datatype.number({min: -180,max: 180}).toString();
+    userContext.vars.end_lat = faker.datatype.number({min: -90,max: 90}).toString();
+    userContext.vars.end_long = faker.datatype.number({min: -180,max: 180}).toString();
+    userContext.vars.rider_name = faker.name.findName();
+    userContext.vars.driver_name = faker.name.findName();
+    userContext.vars.driver_vehicle = faker.vehicle.vehicle();
+    return next();
 }
  
 module.exports = {
-  generateRide
+    generateRide
 };
